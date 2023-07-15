eebef3172f74ad958ca2faf7d8ddd5a598971ace
JinseoKim <eunoia@handong.ac.kr>
2023-07-15 20:29:42 +0900

    Add useHover

---
+++ useHover/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+
+const App = () => {
+  const sayHello = () => console.log("say hello");
+  const title = useHover(sayHello);
+  return (
+    <div className="App">
+      <h1 ref={title}>Hi</h1>
+    </div>
+  );
+};
+
+export default App;
