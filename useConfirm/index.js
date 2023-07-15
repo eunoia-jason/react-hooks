0a9f0228d71d76774674379dbcabd081d36ef79d
JinseoKim <eunoia@handong.ac.kr>
2023-07-15 20:46:34 +0900

    Add useConfirm

--- useConfirm/index.js
+++ useConfirm/index.js
@@ -1,5 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { useConfirm } from "./useConfirm";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
+
+const App = () => {
+  const deleteWorld = () => console.log("Deleting the world...");
+  const abort = () => console.log("Aborted");
+  const confirmDelete = useConfirm("Are you sure", deleteWorld, abort);
+  return (
+    <div className="App">
+      <button onClick={confirmDelete}>Delete the world</button>
+    </div>
+  );
+};
+
+export default App;
