0bf9d526e55b976c8b397dea93bff57a6bceb6f4
JinseoKim <eunoia@handong.ac.kr>
2023-07-15 20:36:30 +0900

    modified

--- useHover/index.js
+++ useHover/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { useHover } from "./useHover";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
--- useTitle/index.js
+++ useTitle/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { useTitle } from "./useTitle";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
