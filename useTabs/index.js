7f7617a87147541f16a2196533d458896ef3d1a6
JinseoKim <eunoia@handong.ac.kr>
2023-07-15 19:49:27 +0900

    Add useTabs

---
+++ useTabs/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+
+const App = () => {
+  const { currentItem, changeItem } = useTabs(0, content);
+  return (
+    <div className="App">
+      {content.map((section, index) => (
+        <button onClick={() => changeItem(index)}>{section.tab}</button>
+      ))}
+      <div>{currentItem.content}</div>
+    </div>
+  );
+};
+
+export default App;
